3b3bae4b5143fd27f3e187d2fa425772daf72d53
akietorna <akietorna@gmail.com>
2025-12-16 07:43:12 +0000

    Make navbar responsive

--- online_driver_booking_system/src/Navbar.jsx
+++ online_driver_booking_system/src/Navbar.jsx
@@ -1,22 +1,36 @@
+import { IoMdClose } from 'react-icons/io';
 import './navbar.css'
-// import logo from './logo.png'
+import { useState } from 'react';
+import { FiChevronDown, FiChevronUp, FiMenu } from 'react-icons/fi';
+
 
 function Navbar() {
+    const [clicked, setClicked] = useState(false);
+    const [hover, setHover] = useState(false);
+
     return(
         <nav className='navBar'>
             <ul className='Navlinks'>
                 <li className='Logo'><a href="">GetADriver</a></li>
-                <li className='Others'> <a href='' target=''>  Others </a> </li>
-                <li className='Others'> 
-                    <a href='' target=''> Register </a> 
-                    <ul className='RegisterDrpdwn'>
-                        <li> <a href="" className='Drop1'>As A Driver</a> </li>
-                        <li> <a href="" className='Drop2'>As A Rider</a> </li>
-                        <li> <a href="" className='Drop3'>As A Customer</a> </li>
+                <div className= {clicked ? 'Close' : 'Menu'}>
+                    <button className='Button' onClick={() => setClicked(!clicked)}> {clicked ? <IoMdClose /> : <FiMenu />}</button>  
+                    <ul className='Others'>                       
+                        <li className='Register'>  
+                            <a href='' target='' onMouseEnter={() => setHover(!hover)} onMouseLeave={() => setHover(!hover)}> Register  
+                            {hover ? <FiChevronUp className='Arrow' /> : <FiChevronDown className='Arrow' /> } </a> 
+                            <ul className='RegisterDrpdwn'>
+                                <li className='Drop1'> <a href="" >As A Driver</a> </li>
+                                <li className='Drop2'> <a href="" >As A Rider</a> </li>
+                                <li className='Drop3'> <a href="" >As A Customer</a> </li>
+                            </ul>
+                        </li>
+                        <li className='Service'> <a href='' target=''>Our Services</a> </li>
+                        <li className='Other'> <a href='' target=''>  Others </a> </li>
+                        <li className='Profile'> <a href='' target=''> Profile </a> </li>
                     </ul>
-                </li>
-                <li className='Others'> <a href='' target=''>Our Services</a> </li>
-                <li className='Others'> <a href='' target=''> Profile </a> </li>
+                </div>
+                
+                
                 
             </ul>
 
